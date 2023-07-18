4bb31687d1d8eb85dd84500ac0ec775598b8154b
Vitsi <lessons019@gmail.com>
2023-07-18 06:21:52 -0700

    task 1 fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,11 @@
 /**
  * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
 	_putchar("_putchar");
-	return(0)
+	_putchar('\n');
+
+	return (0);
 }
