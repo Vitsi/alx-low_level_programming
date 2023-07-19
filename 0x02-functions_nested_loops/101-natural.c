a06fe230f5ef65312b44158967a86758492b49aa
Vitsi <lessons019@gmail.com>
2023-07-19 10:44:02 -0700

    fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,14 +9,15 @@ int main(void)
 {
 	int num, numT, numF, numB, sum;
 
-	numT=3;
-	numF=5;
-
-	numB=1024;
-	sum=0;
+	numT = 3;
+	numF = 5;
+	numB = 1024;
+	sum = 0;
 	
-	for(num = 0; num < numB; num++){
-		if(num % numT == 0|| num % numF == 0){
+	for (num = 0; num < numB; num++)
+	{
+		if (num % numT == 0 || num % numF == 0)
+		{
 			sum += num;
 		}
 	}
