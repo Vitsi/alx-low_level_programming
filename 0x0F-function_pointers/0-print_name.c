5fa0d1cc4f574a89478d735e25c42ec7345dc305
Vitsi <lessons019@gmail.com>
2023-08-27 11:18:00 -0700

    tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,14 +8,10 @@
  *
  * Return: no return.
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char \*name, void (\*f)(char \*))
 {
-    f(name);
+if (name && f)
+{
+f(name);
 }
-int main(void)
-{
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
 }
